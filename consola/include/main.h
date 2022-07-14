273aea00cca33f58463cb1d31be3cfe92914eff7
FacuVerge <facu.verge@gmail.com>
2022-07-14 00:39:41 -0300

    Ordeno logs de consola y arreglo send

--- consola/include/main.h
+++ consola/include/main.h
@@ -12,21 +12,6 @@
 #include <commons/log.h>
 #include <pthread.h>
 #include "utils.h"
-/*
-
-void  mostrar_menu();
-int atender_conexion(int);
-int recibir_operacion_new(int);
-void leer_consola(t_log*);
-void obtener_bitacora(int ID_TRIPULANTE);
-void pausar_planificacion();
-void iniciar_planificacion();
-void leer_consola(t_log*);
-void mostrar_menu();
-void* menu_principal();
-void enviar_un_id(t_log*,uint32_t);
-void enviar_un_string(t_log*, char*);
-void enviar_un_string_y_recibir_respuesta(t_log*, char*, int);*/
 
 instruccion* armar_y_devolver_instruccion (char*);
 int devolver_cantidad_de_instrucciones(char*);
